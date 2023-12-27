bcd5d7f0f3b50302cfb5f1af0b51ef23b291ec32
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2023-12-27 15:28:13 -0300

    Refactor projects source

--- src/main_page/index.jsx
+++ src/main_page/index.jsx
@@ -1,13 +1,14 @@
 import HeroBanner from './hero_banner'
 import Section from './section'
 import react_projects from './react_projects.json'
+import other_projects from './other_projects.json'
 
 export default function MainPage() {
   return (
     <>
       <HeroBanner />
       <Section title={'React Projects'} list={react_projects} />
-      <Section title={'Other Projects'} list={react_projects} />
+      <Section title={'Other Projects'} list={other_projects} />
     </>
   )
 }
